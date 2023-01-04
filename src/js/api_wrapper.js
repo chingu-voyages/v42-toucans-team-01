fbf95d9da5bd6e4b095367359709784fe74a14ba
Christopher James <68414004+gamingbuddhist@users.noreply.github.com>
2023-01-04 13:20:47 -0500

    differentiate public and private methods and variables

--- src/js/api_wrapper.js
+++ src/js/api_wrapper.js
@@ -2,38 +2,45 @@ class ChuckWrapper {
   constructor() {
     this.numItems = 1;
     this.repeat = false;
-    this.seenJokes = new Set();
     this.currentJokes = [];
     this.categories = [];
+    this._seenJokes = new Set();
   }
 
-  async fetchCategories() {
+  async _fetchCategories() {
     let categories = await fetch("https://api.chucknorris.io/jokes/categories");
     this.categories = await categories.json();
   }
 
-  async fetchJoke() {
+  async _fetchJoke() {
     let joke = await fetch("https://api.chucknorris.io/jokes/random");
     return await joke.json();
   }
 
-  async fetchJokeByCategory(category) {
+  async _fetchJokeByCategory(category) {
     let joke = await fetch(`https://api.chucknorris.io/jokes/random?category=${category}`);
     return await joke.json();
   }
 
-  async fetchJokeByQuery(query) {
-    query = query.replace(" ", "+").toLowerCase();
+  async _fetchJokeByQuery(query) {
     let search = await fetch(`https://api.chucknorris.io/jokes/search?query=${query}`);
-    return await search.json();
+    if (search.ok) {
+      return await search.json();
+    } else {
+      return { result: [] };  // handled 400 error but no way to hide console error message on a failed fetch
+    }
+  }
+
+  async getCategories() {
+    await this._fetchCategories();
   }
 
   async getJokes() {
     this.currentJokes = [];
     while (this.currentJokes.length < this.numItems) {
-      let joke = await this.fetchJoke();
-      if (!this.seenJokes.has(joke.id) || this.repeat) {
-        this.seenJokes.add(joke.id);
+      let joke = await this._fetchJoke();
+      if (!this._seenJokes.has(joke.id) || this.repeat) {
+        this._seenJokes.add(joke.id);
         this.currentJokes.push(joke.value);
       }
     }
@@ -42,9 +49,9 @@ class ChuckWrapper {
   async getJokesByCategory(category) {
     this.currentJokes = [];
     while (this.currentJokes.length < this.numItems) {
-      let joke = await this.fetchJokeByCategory(category);
-      if (!this.seenJokes.has(joke.id) || this.repeat) {
-        this.seenJokes.add(joke.id);
+      let joke = await this._fetchJokeByCategory(category);
+      if (!this._seenJokes.has(joke.id) || this.repeat) {
+        this._seenJokes.add(joke.id);
         this.currentJokes.push(joke.value);
       }
     }
@@ -52,7 +59,7 @@ class ChuckWrapper {
 
   async getJokesByQuery(query) {
     this.currentJokes = [];
-    let search = await this.fetchJokeByQuery(query);
+    let search = await this._fetchJokeByQuery(query);
     let jokes = search.result;
     for (let i = 0; i < jokes.length; i++) {
       this.currentJokes.push(jokes[i].value);

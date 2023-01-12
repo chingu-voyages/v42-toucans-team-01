e2a4bbc68e606a22842d1254b3bf488503ccfdbe
Christopher James <68414004+gamingbuddhist@users.noreply.github.com>
2023-01-12 08:56:11 -0500

    add page navigation

---
+++ src/js/scripts.js
@@ -0,0 +1,42 @@
+const mappingElement = document.getElementById("mapping");
+const randomButton = document.getElementById("randomButton");
+const teamButton = document.getElementById("teamButton");
+const randomSection = document.getElementById("randomSection");
+const jokeSection = document.getElementById("jokeSection");
+const teamSection = document.getElementById("teamSection");
+const searchForm = document.getElementById("searchForm");
+const teamFooterButton = document.getElementById("teamFooterButton");
+
+
+const updateMapping = (newMapping) => mappingElement.innerText = newMapping;
+
+const goToRandomPage = () => {
+  updateMapping("RANDOM");
+  randomSection.style.display = "block";
+  teamSection.style.display = "none";
+  jokeSection.style.display = "block";
+  generateButton.click();
+}
+
+const goToSearchPage = () => {
+  updateMapping("SEARCH");
+  randomSection.style.display = "none";
+  teamSection.style.display = "none";
+  jokeSection.style.display = "block";
+}
+
+const goToTeamPage = () => {
+  updateMapping("TEAM");
+  randomSection.style.display = "none";
+  teamSection.style.display = "block";
+  jokeSection.style.display = "none";
+}
+
+randomButton.addEventListener("click", goToRandomPage);
+teamButton.addEventListener("click", goToTeamPage);
+teamFooterButton.addEventListener("click", goToTeamPage);
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  goToSearchPage();
+  getSearchResults();
+});

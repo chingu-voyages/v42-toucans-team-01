bb0903a2ba7279d0b6e55eba1776e26a113a8c8b
Christopher James <68414004+gamingbuddhist@users.noreply.github.com>
2023-01-21 16:49:11 -0500

    fix auto resize

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,15 @@ const jokeSection = document.getElementById("jokeSection");
 const teamSection = document.getElementById("teamSection");
 const teamFooterButton = document.getElementById("teamFooterButton");
 
+const resizeJokes = () => {
+  if (window.innerWidth >= 850 && randomSection.style.display === "block") {
+    jokeSection.style.margin = "15px 86px 25px 35px";
+  } else if (window.innerWidth < 850) {
+    jokeSection.style.margin = "15px 15px 25px 15px";
+  } else {
+    jokeSection.style.margin = "20px 191px 20px 174px";
+  }
+}
 
 const updateBreadcrumb = (newText) => breadcrumbElement.innerText = newText;
 
@@ -26,11 +35,10 @@ const goToPage = (page) => {
   updateBreadcrumb(page.toUpperCase());
   allPages.forEach(section => section.style.display = "none");
   pages[page].forEach(section => section.style.display = "block");
-  if (page === "random") {
-    jokeSection.style.margin = "15px 86px 25px 35px";
-  }
+  resizeJokes();
 }
 
+window.addEventListener("resize", resizeJokes);
 homeButton.addEventListener("click", () => goToPage("home"));
 randomButton.addEventListener("click", () => goToPage("random"));
 teamButton.addEventListener("click", () => goToPage("team"));
